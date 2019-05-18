06fa779adbd8e22e2735e06f7775a486d23a7e66
Adrien Martin <adrien.v.martin@gmail.com>
2019-05-18 17:16:39 -0600

    Update tsconfig and add typechecking to Server file

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 const express = require('express');
-const { connectDB } = require('./config/db');
+const connectDB = require('./config/db');
 import { Request, Response } from 'express';
 
 const app = express();
