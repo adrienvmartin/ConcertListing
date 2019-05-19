75127e2c380fa534ead98faf9d2fffe976b6217c
Adrien Martin <adrien.v.martin@gmail.com>
2019-05-18 20:55:46 -0600

    Add JWT authentication

--- routes/api/users.ts
+++ routes/api/users.ts
@@ -5,6 +5,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 router.post(
@@ -53,7 +55,20 @@ router.post(
 
       await user.save();
 
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 }, (err: string, token: string) => {
+          if (err) { throw err; }
+          res.json({ token });
+        });
+
     } catch (err) {
       console.error(err);
       res.status(500).send('Server error');
