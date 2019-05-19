e8c9be658996ec1d849786a176e889573065d461
Adrien Martin <adrien.v.martin@gmail.com>
2019-05-19 11:51:10 -0600

    Add profile GET route

--- routes/api/profile.ts
+++ routes/api/profile.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from 'express';
+import { SERVER_ERROR_MSG, NO_PROFILE_MSG } from '../../utils/constants';
+import { IUserAuth } from '../../utils/interfaces';
+import { Profile } from '../../models/Profile';
+import { User } from '../../models/User';
 
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-router.get('/', (req: Request, res: Response) => res.send('Profile route'));
+router.get('/me', auth, async (req: IUserAuth, res: Response) => {
+  try {
+    let profile = await Profile.findOne({ user: req.user.id }).populate('user',
+      ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: NO_PROFILE_MSG });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(SERVER_ERROR_MSG);
+  }
+
+});
 
 module.exports = router;
--- utils/constants.ts
+++ utils/constants.ts
@@ -4,5 +4,6 @@ export const NO_TOKEN_MSG = 'No token found, authorization denied';
 export const INVALID_TOKEN_MSG = 'Invalid token';
 
 export const EXISTING_USER_MSG = 'User already exists';
+export const NO_PROFILE_MSG = 'There is no profile for this user';
 
 export const INVALID_CREDENTIALS_MSG = 'Invalid credentials';
