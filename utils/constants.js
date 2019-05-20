9ad8bde8aaac820c34cbd684083846a6e874b27a
Adrien Martin <adrien.v.martin@gmail.com>
2019-05-19 23:50:19 -0600

    Change export style for constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,10 +1,9 @@
-export const SERVER_ERROR_MSG = 'Server error';
-
-export const NO_TOKEN_MSG = 'No token found, authorization denied';
-export const INVALID_TOKEN_MSG = 'Invalid token';
-
-export const EXISTING_USER_MSG = 'User already exists';
-export const NO_PROFILE_MSG = 'There is no profile for this user';
-
-export const INVALID_CREDENTIALS_MSG = 'Invalid credentials';
-export const STATUS_REQUIRED_MSG = 'Status is required';
+module.exports = {
+  SERVER_ERROR_MSG: 'Server error',
+  NO_TOKEN_MSG: 'No token found, authorization denied',
+  INVALID_TOKEN_MSG: 'Invalid token',
+  EXISTING_USER_MSG: 'User already exists',
+  NO_PROFILE_MSG: 'There is no profile for this user',
+  INVALID_CREDENTIALS_MSG: 'Invalid credentials',
+  STATUS_REQUIRED_MSG: 'Status is required',
+};
