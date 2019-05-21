1713be1e8ccc6773dcad0ce21bf7e362db846e75
Adrien Martin <adrien.v.martin@gmail.com>
2019-05-21 16:00:16 -0600

    Add form to Register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords don\'t match');
+    } else {
+      console.log(formData);
+    }
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user" />
+        Create Your Account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" onChange={e => onChange(e)} />
+        </div>
+        <div className="form-group">
+          <input type="password" placeholder="Password" name="password" onChange={e => onChange(e)} />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+      Already have an account? <Link to='/login'>Sign In </Link></p>
+    </Fragment>
   );
 };
 
