1c10b03b5feea9079f06fe6f83466fd49085fc63
Adrien Martin <adrien.v.martin@gmail.com>
2019-05-21 16:12:16 -0600

    Update Register fform with new inputs

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -17,50 +17,64 @@ const Register = () => {
   const onSubmit = e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords don\'t match');
+      console.log("Passwords don't match");
     } else {
       console.log(formData);
     }
   }
 
-  return (
-    <Fragment>
-      <h1 className="large text-primary">Sign Up</h1>
-      <p className="lead">
-        <i className="fas fa-user" />
-        Create Your Account
+    return (
+      <Fragment>
+        <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead">
+          <i className="fas fa-user" />
+          Create Your Account
       </p>
-      <form className="form" onSubmit={e => onSubmit(e)}>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            value={name}
-            onChange={e => onChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" onChange={e => onChange(e)} />
-        </div>
-        <div className="form-group">
-          <input type="password" placeholder="Password" name="password" onChange={e => onChange(e)} />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-            onChange={e => onChange(e)}
-          />
-        </div>
-        <input type='submit' className='btn btn-primary' value='Register' />
-      </form>
-      <p className='my-1'>
-      Already have an account? <Link to='/login'>Sign In </Link></p>
-    </Fragment>
-  );
-};
+        <form className="form" onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={e => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={e => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="password2"
+              value={password2}
+              onChange={e => onChange(e)}
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <Link to="/login">Sign In </Link>
+        </p>
+      </Fragment>
+    );
+  };
 
 export default Register;
