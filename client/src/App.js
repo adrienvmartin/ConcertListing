dcd431eb33d8bb13c313de45cf243c13112ebd64
Adrien Martin <adrien.v.martin@gmail.com>
2019-05-21 14:07:30 -0600

    Fix import problem with Register component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from ' ./components/auth/Register';
+import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
 
