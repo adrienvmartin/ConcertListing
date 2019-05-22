043d916162dff971600e8bdf4e5367c5ae22a461
Adrien Martin <adrien.v.martin@gmail.com>
2019-05-22 11:13:48 -0600

    Fix formatting in db config file

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const config = require('config');
+
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
     });
 
     console.log('MongoDB successfully connected');
