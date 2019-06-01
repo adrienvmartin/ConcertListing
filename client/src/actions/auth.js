04ddcc07507f4cc330055be2e71ae1f28b35c970
Adrien Martin <adrien.v.martin@gmail.com>
2019-06-01 16:11:34 -0600

    Add USER_LOADED and AUTH_ERROR types

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from './types';
+
+export const loadUser = () => async dispatch => {
+  
+}
 
 export const register = ({ name, email, password }) => async dispatch => {
   const config = {
