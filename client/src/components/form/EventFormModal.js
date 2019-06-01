8be40e37d07d41a84cd681d1d23378b2a86bb454
Adrien Martin <adrien.v.martin@gmail.com>
2019-05-31 22:34:44 -0600

    Add PropTypes to form modal

--- client/src/components/form/EventFormModal.js
+++ client/src/components/form/EventFormModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
+import PropTypes from 'prop-types';
 import CreateEventForm from './NewEvent';
 
 const EventFormModal = ({ open, onRequestClose }) => {
@@ -8,6 +9,11 @@ const EventFormModal = ({ open, onRequestClose }) => {
       <CreateEventForm />
     </Dialog>
   );
+};
+
+EventFormModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onRequestClose: PropTypes.func.isRequired
 };
 
 export default EventFormModal;
