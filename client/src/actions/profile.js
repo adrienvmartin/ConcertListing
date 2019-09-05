66214d801fb72718dcb1525d6b7bb2b967436eb0
Adrien Martin <adrien.v.martin@gmail.com>
2019-09-04 21:59:29 -0600

    Add action creators for adding and deleting shows

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, ADD_SHOW } from './types';
+import { setAlert } from './alert';
 
 export const getCurrentProfile = () => async dispatch => {
   try {
@@ -8,6 +9,54 @@ export const getCurrentProfile = () => async dispatch => {
       type: GET_PROFILE,
       payload: res.data
     });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+export const addShow = formData => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.put('/api/profile/shows', formData, config);
+
+    dispatch({
+      type: ADD_SHOW,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Show added', 'success'));
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+export const deleteShow = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/shows/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Event removed', 'success'));
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
