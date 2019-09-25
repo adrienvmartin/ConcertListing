28693a9e33acc9776b1cdb67b8c1e05d3ff3c145
Adrien Martin <adrien.v.martin@gmail.com>
2019-09-24 18:25:50 -0600

    Update Profile model

--- models/Profile.js
+++ models/Profile.js
@@ -5,6 +5,34 @@ const ProfileSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
   },
+  events: [
+    {
+      bands: {
+        headliner: {
+          type: String,
+          required: true,
+        },
+        openers: [
+          {
+            type: String,
+            required: false,
+          },
+        ],
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      venue: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
 const Profile = mongoose.model('profile', ProfileSchema);
