437e5b8f52199492a7a770cb25ef3b47ae24a011
Adrien Martin <adrien.v.martin@gmail.com>
2019-09-24 18:26:51 -0600

    Move event actions into profile section

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_ERROR, ADD_SHOW, UPDATE_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_ERROR, ADD_SHOW, DELETE_SHOW } from './types';
 import { setAlert } from './alert';
 
 export const getCurrentProfile = () => async dispatch => {
@@ -9,6 +9,69 @@ export const getCurrentProfile = () => async dispatch => {
       type: GET_PROFILE,
       payload: res.data
     });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+export const loadEvents = async dispatch => {
+  try {
+    const res = await axios.get('/api/profile/events');
+
+    dispatch({
+      type: 'LOAD_EVENTS',
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: 'ERROR',
+    });
+  }
+};
+
+export const createEvent = ({ formData }) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.put(`/api/profile/events`, formData, config);
+
+    dispatch({
+      type: ADD_SHOW,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Show added', 'success'));
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+export const deleteEvent = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/events/${id}`);
+
+    dispatch({
+      type: DELETE_SHOW,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Event Removed', 'success'));
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
