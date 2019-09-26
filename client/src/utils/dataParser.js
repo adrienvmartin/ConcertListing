e1c6ba0bec94a4ce0af03d8ac0cad743c30fd027
Adrien Martin <adrien.v.martin@gmail.com>
2019-09-25 21:09:26 -0600

    Created preliminary data structure format

--- client/src/utils/dataParser.js
+++ client/src/utils/dataParser.js
@@ -1,4 +1,5 @@
-export const bandSplitter = bands => { // Needs to be one band at a time?
+export const bandSplitter = bands => {
+  // Needs to be one band at a time?
   const bandList = [];
   const headliner = bands.headliner;
   const openers = bands.openers;
@@ -20,6 +21,72 @@ export const duplicateCheck = bands => {
   });
 };
 
+export const instanceCalculator = bandArray => {
+  const count = {};
+  bandArray.forEach(b => {
+    count[b] = (count[b] || 0) + 1;
+  });
+};
+
 export const yearSplitter = date => {
   return date.toString().slice(0, 4);
 };
+
+/* Intended Data Structure:
+
+profile: {
+  events: [
+    {
+      bands: {
+        headliner: 'Headliner',
+        openers: ['Array', 'of', 'openers']
+      },
+      city: 'City',
+      venue: 'Venue',
+      date: 'date'
+    },
+    {
+      bands: {
+        headliner: 'Other Headliner',
+        openers: ['Second', 'Array']
+      },
+      city: 'Some Town',
+      venue: 'Some Place',
+      date: 'Another Date'
+    }
+  ],
+  bands: [
+    {
+      name: 'Headliner',
+      instances: [instance1],
+      instanceCount: 1,
+      cities: 1,
+      venues: 1
+    },
+    {
+      name: 'Array',
+      instanceCount: 2,
+      instances: [instance1, instance2],
+      cities: 2,
+      venues: 2
+    }
+  ],
+  cities: [
+    {
+      name: 'City',
+      instances: [instance1],
+      instanceCount: 1,
+      venues: 1,
+      bands: ['Headliner', 'Array', 'Of', 'Openers']
+    },
+    {
+      name: 'Some Town',
+      instances: [instance1],
+      instanceCount: 2,
+      venues: 1,
+      bands: ['Other Headliner', 'Second', 'Array']
+    }
+  ]
+};
+
+*/
