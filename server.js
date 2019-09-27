706ea6c2e97e574ba4aff09301d9342d6837adb4
Adrien Martin <adrien.v.martin@gmail.com>
2019-09-26 22:40:10 -0600

    Add events API to server

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => res.send('API running'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/events', require('./routes/api/events'));
 
 const PORT = process.env.PORT || 5000;
 
