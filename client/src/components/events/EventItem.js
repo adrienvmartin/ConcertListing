1630411b06320393578e8f280a61ab0863b206ad
Adrien Martin <adrien.v.martin@gmail.com>
2019-09-28 14:25:32 -0600

    Separate Event and EventItem components

--- client/src/components/events/EventItem.js
+++ client/src/components/events/EventItem.js
@@ -1,56 +1,42 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { loadEvents } from '../../actions/profile';
-import Spinner from '../layout/Spinner';
+import { deleteEvent } from '../../actions/profile';
 
-const Event = ({ loadEvents, event: { events, loading } }) => {
-  useEffect(
-    () => {
-      loadEvents();
-    },
-    [loadEvents]
-  );
-  const shows = events.map(ev => (
-    <tr key={ev._id}>
-      <td>{ev.headliner}></td>
-      <td>{ev.bands}</td>
-      <td>{ev.venue}</td>
-      <td>{ev.city}</td>
-      <td>{ev.date}</td>
+const EventItem = ({
+  event: {
+    _id,
+    bands: { headliner, openers },
+    city,
+    venue,
+    date
+  },
+  deleteEvent
+}) => {
+  return (
+    <tr key={_id}>
+      <td>{headliner}</td>
+      <td>{openers}</td>
+      <td>{venue}</td>
+      <td>{city}</td>
+      <td>{date}</td>
       <td>
-        <button className="btn btn-danger">Delete</button>
+        <button
+          onClick={() => deleteEvent(_id)}
+          className="btn btn-danger">Delete</button>
       </td>
     </tr>
-  ));
-  return loading ? (
-    <Spinner />
-  ) : (
-    <Fragment>
-      <h2 className="my-2">Event List</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Headliner</th>
-            <th>Openers</th>
-            <th>Venue</th>
-            <th>City</th>
-            <th>Date</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>{shows}</tbody>
-      </table>
-    </Fragment>
   );
 };
 
 Event.propTypes = {
-  event: PropTypes.array.isRequired
+  event: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  deleteEvent: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-  event: state.events,
+  auth: state.auth
 });
 
-export default connect(mapStateToProps, { loadEvents })(Event);
+export default connect(mapStateToProps, { deleteEvent })(EventItem);
---
+++ client/src/components/events/Events.js
@@ -0,0 +1,57 @@
+import React, { Fragment, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import EventItem from './EventItem';
+import { loadEvents } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
+
+const Events = ({ loadEvents, event: { events, loading } }) => {
+  useEffect(
+    () => {
+      loadEvents();
+    },
+    [loadEvents]
+  );
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Events</h1>
+      <Fragment>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Headliner</th>
+              <th>Openers</th>
+              <th>Venue</th>
+              <th>City</th>
+              <th>Date</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {events.map(ev => (
+              <EventItem key={ev._id} event={ev} />
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
+      <div className="events" />
+    </Fragment>
+  );
+};
+
+Events.propTypes = {
+  loadEvents: PropTypes.func.isRequired,
+  event: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  event: state.event
+});
+
+export default connect(
+  mapStateToProps,
+  { loadEvents }
+)(Events);
