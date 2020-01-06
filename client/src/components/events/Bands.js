6b5c47b5040c11ebb99b87e7bf1fab0cbd0ee37a
Adrien Martin <adrien.v.martin@gmail.com>
2020-01-05 18:30:38 -0600

    Move Material-UI table to its own component and import into Bands component

--- client/src/components/events/Bands.js
+++ client/src/components/events/Bands.js
@@ -1,125 +1,12 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner';
 import { loadBands } from '../../actions/event';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TableSortLabel,
-  TablePagination,
-  TableContainer,
-  Paper,
-  makeStyles
-} from '@material-ui/core';
+import ItemTable from './ItemTable';
+import Spinner from '../layout/Spinner';
 
-const desc = (a, b, orderBy) => {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-};
-
-const stableSort = (array, cmp) => {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = cmp(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map(el => el[0]);
-};
-
-const getSorting = (order, orderBy) => {
-  return order === 'desc'
-    ? (a, b) => desc(a, b, orderBy)
-    : (a, b) => -desc(a, b, orderBy);
-};
-
-const headCells = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Band Name' },
-  { id: 'instances', numeric: false, disablePadding: false, label: 'Times Seen' },
-];
-
-const EnhancedTableHead = props => {
-  const { classes, order, orderBy, onRequestSort } = props;
-  const createSortHandler = property => event => {
-    onRequestSort(event, property);
-  };
-  return (
-    <TableHead>
-      <TableRow>
-        {headCells.map(h => (
-          <TableCell
-            key={h.id}
-            padding={h.disablePadding ? 'none' : 'default'}
-            sortDirection={orderBy === h.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === h.id}
-              direction={orderBy === h.id ? order : 'asc'}
-              onClick={createSortHandler(h.id)}
-            >
-              {h.label}
-              {orderBy === h.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-};
-
-EnhancedTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
-EnhancedTableHead.defaultProps = {
-  classes: {},
-  onRequestSort: () => {},
-  order: 'asc',
-  orderBy: 'name',
-  rowCount: '25',
-};
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%'
-  },
-  paper: {
-    width: '100%',
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-    margin: 10,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    top: 20,
-    width: 1
-  }
-}));
+const blankMessage = 'You have not seen any bands yet.';
+const headerTitle = 'Bands';
 
 const Bands = ({ loadBands, bands, loading }) => {
   useEffect(
@@ -128,87 +15,19 @@ const Bands = ({ loadBands, bands, loading }) => {
     },
     [loadBands]
   );
-  const classes = useStyles();
-  const [order, setOrder] = useState('asc');
-  const [orderBy, setOrderBy] = useState('name'); // when you click, does it change setOrderBy?
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(25);
-
-  const rows = [];
-
-  bands.forEach(b => {
-    rows.push({ name: b.name, instances: b.instances });
-  });
-
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
 
   return loading ? (
     <Spinner />
   ) : (
-    <div className={classes.root}>
-      <br />
-      <h1>Bands</h1>
-      {bands.length > 0 ? (
-        <Paper className={classes.paper}>
-          <TableContainer>
-            <Table className={classes.table} size='small'>
-              <EnhancedTableHead
-                classes={classes}
-                order={order}
-                orderBy={orderBy}
-                onRequestSort={handleRequestSort}
-                rowCount={rows.length}
-              />
-              <TableBody>
-                {stableSort(rows, getSorting(order, orderBy))
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((row, index) => {
-                    const labelId = `enhanced-table-${index}`;
-
-                    return (
-                      <TableRow hover key={row.name}>
-                        <TableCell
-                          component="th"
-                          id={labelId}
-                          scope="row"
-                          padding="none"
-                        >
-                          {row.name}
-                        </TableCell>
-                        <TableCell>{row.instances}</TableCell>
-                      </TableRow>
-                    );
-                  })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[10, 25, 50, 100]}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-          />
-        </Paper>
-      ) : (
-        <Fragment>You have not seen any bands yet.</Fragment>
-      )}
-    </div>
+    <Fragment>
+      <ItemTable
+        data={bands}
+        loadFunction={loadBands}
+        loading={loading}
+        blankMessage={blankMessage}
+        headerTitle={headerTitle}
+      />
+    </Fragment>
   );
 };
 
---
+++ client/src/components/events/ItemTable.js
@@ -0,0 +1,229 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  TablePagination,
+  TableContainer,
+  Paper,
+  makeStyles
+} from '@material-ui/core';
+
+const desc = (a, b, orderBy) => {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+};
+
+const stableSort = (array, cmp) => {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+};
+
+const getSorting = (order, orderBy) => {
+  return order === 'desc'
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
+};
+
+const headCells = [
+  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
+  {
+    id: 'instances',
+    numeric: false,
+    disablePadding: false,
+    label: 'Times Seen'
+  }
+];
+
+const EnhancedTableHead = props => {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = property => event => {
+    onRequestSort(event, property);
+  };
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map(h => (
+          <TableCell
+            key={h.id}
+            padding={h.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === h.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === h.id}
+              direction={orderBy === h.id ? order : 'asc'}
+              onClick={createSortHandler(h.id)}
+            >
+              {h.label}
+              {orderBy === h.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired
+};
+
+EnhancedTableHead.defaultProps = {
+  classes: {},
+  onRequestSort: () => {},
+  order: 'asc',
+  orderBy: 'name',
+  rowCount: '25'
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%'
+  },
+  paper: {
+    width: '100%',
+    marginBottom: theme.spacing(2)
+  },
+  table: {
+    minWidth: 750,
+    margin: 10
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1
+  }
+}));
+
+const ItemTable = ({
+  loadFunction,
+  data,
+  blankMessage,
+  headerTitle
+}) => {
+  useEffect(
+    () => {
+      loadFunction();
+    },
+    [loadFunction]
+  );
+  const classes = useStyles();
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('name'); // when you click, does it change setOrderBy?
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(25);
+
+  const rows = [];
+
+  data.forEach(d => {
+    rows.push({ name: d.name, instances: d.instances });
+  });
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <div className={classes.root}>
+      <br />
+      {data.length > 0 ? (
+        <Paper className={classes.paper}>
+          <h1>{headerTitle}</h1>
+          <TableContainer>
+            <Table className={classes.table} size="small">
+              <EnhancedTableHead
+                classes={classes}
+                order={order}
+                orderBy={orderBy}
+                onRequestSort={handleRequestSort}
+                rowCount={rows.length}
+              />
+              <TableBody>
+                {stableSort(rows, getSorting(order, orderBy))
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row, index) => {
+                    const labelId = `enhanced-table-${index}`;
+
+                    return (
+                      <TableRow hover key={row.name}>
+                        <TableCell
+                          component="th"
+                          id={labelId}
+                          scope="row"
+                          padding="none"
+                        >
+                          {row.name}
+                        </TableCell>
+                        <TableCell>{row.instances}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 50, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        </Paper>
+      ) : (
+        <Fragment>
+          <h1>{headerTitle}</h1>
+          {blankMessage}
+        </Fragment>
+      )}
+    </div>
+  );
+};
+
+ItemTable.propTypes = {
+  loadFunction: PropTypes.func.isRequired,
+  data: PropTypes.array.isRequired,
+  blankMessage: PropTypes.string.isRequired
+};
+
+export default ItemTable;
