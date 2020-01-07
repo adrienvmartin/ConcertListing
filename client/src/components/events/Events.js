207676335a2957d5b7b7df7d1f48edc8ee7e4206
Adrien Martin <adrien.v.martin@gmail.com>
2020-01-07 10:47:51 -0600

    Add stats to main event listing

--- client/src/components/events/Events.js
+++ client/src/components/events/Events.js
@@ -1,16 +1,21 @@
 import React, { Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { loadEvents } from '../../actions/event';
+import { loadEvents, getStats } from '../../actions/event';
 import Spinner from '../layout/Spinner';
 import ListingsTable from './ListingsTable';
 
-const Events = ({ loadEvents, event: { events, loading } }) => {
+const Events = ({
+  loadEvents,
+  event: { events, loading, stats },
+  getStats
+}) => {
   useEffect(
     () => {
       loadEvents();
+      getStats();
     },
-    [loadEvents]
+    [loadEvents, getStats]
   );
 
   return loading ? (
@@ -20,7 +25,11 @@ const Events = ({ loadEvents, event: { events, loading } }) => {
       <h1 className="large text-primary">Events</h1>
       {events.length > 0 ? (
         <Fragment>
-         <ListingsTable data={events} />
+          Welcome to your listings page! You've been to{' '}
+          <b>{stats.eventCount} events</b> and have seen{' '}
+          <b>{stats.bands} bands</b> at <b>{stats.venues} venues</b> in{' '}
+          <b>{stats.cities} cities</b>.
+          <ListingsTable data={events} />
         </Fragment>
       ) : (
         <Fragment>You have not created any events yet.</Fragment>
@@ -31,7 +40,17 @@ const Events = ({ loadEvents, event: { events, loading } }) => {
 
 Events.propTypes = {
   loadEvents: PropTypes.func.isRequired,
-  event: PropTypes.object.isRequired
+  event: PropTypes.object.isRequired,
+  stats: PropTypes.object
+};
+
+Events.defaultProps = {
+  stats: {
+    eventCount: 0,
+    bands: 0,
+    cities: 0,
+    venues: 0
+  }
 };
 
 const mapStateToProps = state => ({
@@ -40,5 +59,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { loadEvents }
+  { loadEvents, getStats }
 )(Events);
