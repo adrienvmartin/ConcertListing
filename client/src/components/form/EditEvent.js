b2b52790b58d82080fb75a4ff257ba90e56600d0
Adrien Martin <adrien.v.martin@gmail.com>
2020-01-06 22:14:31 -0600

    Create form and modal for editing events

---
+++ client/src/components/form/EditEvent.js
@@ -0,0 +1,137 @@
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { TextField, Button } from '@material-ui/core';
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import { Link } from 'react-router-dom';
+import { editEvent } from '../../actions/event';
+
+const EditEvent = ({
+  defaultId,
+  defaultHeadliner,
+  defaultOpeners,
+  defaultCity,
+  defaultVenue,
+  defaultDate,
+  onSubmit,
+}) => {
+  const [formData, setFormData] = useState({
+    id: defaultId,
+    bands: {
+      headliner: defaultHeadliner,
+      openers: defaultOpeners,
+    },
+    city: defaultCity,
+    venue: defaultVenue,
+    date: defaultDate
+  });
+
+  const {
+    bands: { headliner, openers },
+    city,
+    venue,
+    date
+  } = formData;
+
+  const [selectedDate, setSelectedDate] = useState(new Date(date));
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onChangeBands = e => {
+    setFormData({
+      ...formData,
+      bands: { ...formData.bands, [e.target.name]: e.target.value }
+    });
+  };
+
+  const handleDateChange = date => {
+    setSelectedDate(date);
+    setFormData({
+      ...formData,
+      date: date
+    });
+  };
+
+  const styles = {
+    minWidth: 500
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Edit Event</h1>
+      <small>* = required field</small>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <TextField
+            type="text"
+            placeholder="* Headliner"
+            name="headliner"
+            value={headliner || ''}
+            onChange={e => onChangeBands(e)}
+            required
+            style={styles}
+          />
+        </div>
+        <div className="form-group">
+          <TextField
+            type="text"
+            placeholder="Openers (separated by commas)"
+            name="openers"
+            value={openers || ''}
+            onChange={e => onChangeBands(e)}
+            style={styles}
+          />
+        </div>
+        <div className="form-group">
+          <TextField
+            type="text"
+            placeholder="* Venue"
+            name="venue"
+            value={venue}
+            onChange={e => onChange(e)}
+            required
+            style={styles}
+          />
+        </div>
+        <div className="form-group">
+          <TextField
+            type="text"
+            placeholder="* City"
+            name="city"
+            value={city}
+            onChange={e => onChange(e)}
+            required
+            style={styles}
+          />
+        </div>
+        <div className="form-group">
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              margin="normal"
+              id="date-picker"
+              label="Select A Date"
+              format="yyyy/MM/dd"
+              value={selectedDate}
+              onChange={handleDateChange}
+              style={styles}
+            />
+          </MuiPickersUtilsProvider>
+        </div>
+        <Button variant="contained" color="primary" onClick={() => onSubmit(formData.id, formData)}>Submit</Button>
+        <Link className="btn btn-light my-1" to="/dashboard">
+          Dashboard
+        </Link>
+      </form>
+    </Fragment>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  onSubmit: (id, data) => dispatch(editEvent(id, data)),
+});
+
+export default connect(null, mapDispatchToProps)(EditEvent);
---
+++ client/src/components/form/EditModal.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Dialog } from '@material-ui/core';
+import EditEvent from './EditEvent';
+
+const EditModal = ({
+  open,
+  onClose,
+  showInfo,
+  loading,
+  closeModal,
+  data,
+  onSubmit,
+}) => {
+  return (
+    <Dialog
+      open={open}
+      children={EditEvent}
+      title={'Edit Event'}
+      onClose={onClose}
+    >
+      <EditEvent
+        defaultId={data.id}
+        defaultHeadliner={data.headliner}
+        defaultOpeners={data.openers}
+        defaultCity={data.city}
+        defaultVenue={data.venue}
+        defaultDate={data.date}
+      />
+    </Dialog>
+  );
+};
+
+export default EditModal;
