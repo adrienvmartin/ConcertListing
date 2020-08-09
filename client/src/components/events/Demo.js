49ba149d3285629675bb2f7e539e7489defced7d
Adrien Martin <adrien.v.martin@gmail.com>
2020-08-09 17:37:51 -0600

    Change parameter

--- client/src/components/events/Demo.js
+++ client/src/components/events/Demo.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ListingsTable from './ListingsTable';
 
-const data = [
-  {
-    _id: 1,
-    bands: {
-      headliner: "Headlining Band",
-      openers: "Opener 1, Opener 2"
-    },
-    city: "London",
-    venue: "The Venue",
-    date: "2000-01-01"
-  },
-];
-
-const Demo = () => {
+const Demo = (data) => {
   return (
     <div>
       <div className="landing-inner">
