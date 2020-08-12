e527ed3aa5eb95b89d931c56dfb0c7839b678dfc
Adrien Martin <adrien.v.martin@gmail.com>
2020-08-12 09:29:33 -0600

    Add mock data

--- client/src/components/events/Demo.js
+++ client/src/components/events/Demo.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import ListingsTable from './ListingsTable';
+import ItemTable from './ItemTable';
 
-const Demo = (data) => {
+const data = [
+  {
+    _id: 1,
+    bands: {
+      headliner: "Pantera",
+      openers: "Skrape, Morbid Angel",
+    },
+    city: "Saskatoon",
+    venue: "Saskatchewan Place",
+    date: "2001-07-31"
+  },
+  {
+    _id: 2,
+    bands: {
+      headliner: "Def Leppard",
+      openers: "Ricky Warwick",
+    },
+    city: "Saskatoon",
+    venue: "Saskatchewan Place",
+    date: "2003-09-12"
+  }
+];
+
+const bands = [
+  {
+    name: "Pantera",
+    instances: 1,
+  },
+  {
+    name: "Skrape",
+    instances: 1
+  }
+]
+
+const Demo = () => {
   return (
     <div>
       <div className="landing-inner">
@@ -9,6 +44,8 @@ const Demo = (data) => {
       This is a demo. Try it out!
       <br />
         <ListingsTable data={data} />  
+        <br />
+        <ItemTable data={bands} headerTitle={"Bands"} />
       </div>  
     </div>
   );
