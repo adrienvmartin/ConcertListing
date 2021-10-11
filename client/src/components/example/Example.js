fe671c6f9de25de901fb3107d219de5a553ff8ff
Adrien Martin <adrien.v.martin@gmail.com>
2021-10-11 08:29:34 -0600

    Add props to Example component

--- client/src/components/example/Example.js
+++ client/src/components/example/Example.js
@@ -1,10 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { loadExamples } from '../../actions/event';
 import ListingsTable from '../events/ListingsTable';
 
-export const Example = () => {
+const Example = ({ examples }) => {
+  useEffect(
+    () => {
+      loadExamples();
+    },
+    [loadExamples]
+  )
   return (
     <Fragment>
-      Example Page
+      <ListingsTable data={examples} />
     </Fragment>
   )
 }
+
+const mapStateToProps = state => ({
+  examples: state.examples
+})
+
+export default connect(
+  mapStateToProps, { loadExamples })(Example);
